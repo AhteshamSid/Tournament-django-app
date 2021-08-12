96977d7e0802f255319479189cfe1837f64f716e
AhteshamSid <78630600+AhteshamSid@users.noreply.github.com>
2021-08-12 12:20:06 +0530

    Update views.py

--- app/views.py
+++ app/views.py
@@ -264,3 +264,52 @@ def DeletePoint(request, pk):
 def admin_logout(request):
     logout(request)
     return redirect('home')
+
+
+
+
+
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'sign-up.html')
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        # print(form.errors.as_data())
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            current_site = get_current_site(request)
+            print(current_site)
+            mail_subject = 'Activate your account.'
+            message = render_to_string('account-active.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            # print(message)
+            email_from = settings.EMAIL_HOST_USER
+            recipient_list = [user.email, ]
+            # send_mail(mail_subject, message, email_from, recipient_list)# email.send()
+            msg = EmailMessage(mail_subject, message, to=recipient_list)
+            msg.send()
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'sign-up.html', {'form': form})
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = UserModel._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        return redirect('home')
+    else:
+        return HttpResponse('Activation link is invalid!')
